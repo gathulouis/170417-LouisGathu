4ff853364db27a3f65012466f5b4efd691c13223
Louis Gathu <109313994+gathulouis@users.noreply.github.com>
2023-10-05 17:45:13 +0300

    new

---
+++ ass1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   cout<<"Welcome to the wonderful world of c++!"<<endl;
+   cout<<"This is not my first c++ output statement"<<endl;
+   cout<<"Soon I will learn how to input data into my program"<<endl;
+  
+
+
+   return 0;
+}
